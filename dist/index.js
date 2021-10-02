61fa1c513dc37737b99989989226c30b2f024fdf
Luis Puentes <luispuentesvega@gmail.com>
2021-10-02 12:43:54 +0700

    install express and typescript

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (_, res) => res.status(200).send('Hellow World from Here!'));
+app.listen(port, () => console.log(`Server started listening to port ${port}`));
+//# sourceMappingURL=index.js.map
