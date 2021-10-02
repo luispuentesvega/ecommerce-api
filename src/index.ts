d2742e3dfd30eac93c8308b2f3bd3609f99acee9
Luis Puentes <luispuentesvega@gmail.com>
2021-10-02 12:57:41 +0700

    install and config eslint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (_, res) => res.status(200).send('Hellow World from Here!'));
+app.get('/', (_, res) => res.status(200).send('Hello World from Here!'));
 
 app.listen(port, () => console.log(`Server started listening to port ${port}`));
